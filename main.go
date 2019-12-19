b0d8d921a2ea0eb49d414a794fbd54e8362ac9bb
wangjiawen <wangjiawen@dtas.com>
2019-12-19 15:33:38 +0800

    go blog

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"github.com/DeanThompson/ginpprof"
+	"github.com/gin-gonic/gin"
+	"blogger/dal/db"
+	"blogger/controller"
+	//"fmt"
+	//"os"
+	//"github.com/DeanThompson/ginpprof"
+	//"blogger/controller"
+)
+
+
+//func main() {
+//	r := gin.Default()
+//	r.GET("/ping", func(c *gin.Context) {
+//		c.JSON(200, gin.H{
+//			"message": "pong",
+//		})
+//	})
+//	r.Run() // listen and serve on 0.0.0.0:8080
+//}
+
+
+func main(){
+	router := gin.Default()
+
+	dns := "root:@tcp(127.0.0.1:3306)/blogger?parseTime=true"
+	err := db.Init(dns)
+	if err != nil{
+		panic(err)
+	}
+
+	//fmt.Println(os.Args)
+	ginpprof.Wrapper(router)
+	router.Static("/static/", "src/blogger/static")
+	router.LoadHTMLGlob("src/blogger/views/*")
+
+	router.GET("/", controller.IndexHandle)
+	//发布文章页面
+	router.GET("/article/new/", controller.NewArticle)
+	//文章提交接口
+	router.POST("/article/submit/", controller.ArticleSubmit)
+	//文章详情页
+	router.GET("/article/detail/", controller.ArticleDetail)
+
+	//文件上传接口
+	router.POST("/upload/file/", controller.UploadFile)
+
+	//留言页面
+	router.GET("/leave/new/", controller.LeaveNew)
+	//关于我页面
+	router.GET("/about/me/", controller.AboutMe)
+
+	//文章评论相关
+	router.POST("/comment/submit/", controller.CommentSubmit)
+
+	//留言相关
+	router.POST("/leave/submit/", controller.LeaveSubmit)
+	//分类下面的文章列表
+	router.GET("/category/", controller.CategoryList)
+	router.Run(":8080")
+}
