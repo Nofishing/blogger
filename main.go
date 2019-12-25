e37c986c3585d7f4295adaf0bbdcb7f149cb6750
wangjiawen <wangjiawen@dtas.com>
2019-12-25 11:04:34 +0800

    update

--- main.go
+++ main.go
@@ -1,55 +1,56 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/DeanThompson/ginpprof"
+	//"github.com/gin-gonic/gin"
+	//"github.com/DeanThompson/ginpprof"
 	"blogger/dal/db"
-	"blogger/controller"
-	//"fmt"
+	//"blogger/controller"
+	"fmt"
 	//"os"
 )
 
 
 
 func main(){
-	router := gin.Default()
+	//router := gin.Default()
 
 	//dns := "root:@tcp(127.0.0.1:3306)/blogger?parseTime=true"
 	dns := "root:oneinstack@tcp(localhost:3306)/blogger?parseTime=true"
 	err := db.Init(dns)
-	if err != nil{
-		panic(err)
-	}
-
-	//fmt.Println(os.Args)
-	ginpprof.Wrapper(router)
-	//router.Static("/static/", "src/blogger/static")
-	//router.LoadHTMLGlob("src/blogger/views/*")
-	router.Static("/static/", "./static")
-	router.LoadHTMLGlob("views/*")
-
-	router.GET("/", controller.IndexHandle)
-	//发布文章页面
-	router.GET("/article/new/", controller.NewArticle)
-	//文章提交接口
-	router.POST("/article/submit/", controller.ArticleSubmit)
-	//文章详情页
-	router.GET("/article/detail/", controller.ArticleDetail)
-
-	//文件上传接口
-	router.POST("/upload/file/", controller.UploadFile)
-
-	//留言页面
-	router.GET("/leave/new/", controller.LeaveNew)
-	//关于我页面
-	router.GET("/about/me/", controller.AboutMe)
-
-	//文章评论相关
-	router.POST("/comment/submit/", controller.CommentSubmit)
-
-	//留言相关
-	router.POST("/leave/submit/", controller.LeaveSubmit)
-	//分类下面的文章列表
-	router.GET("/category/", controller.CategoryList)
-	router.Run(":8080")
+	fmt.Println("f1 failed:", err)
+	//if err != nil{
+	//	panic(err)
+	//}
+	//
+	////fmt.Println(os.Args)
+	//ginpprof.Wrapper(router)
+	////router.Static("/static/", "src/blogger/static")
+	////router.LoadHTMLGlob("src/blogger/views/*")
+	//router.Static("/static/", "./static")
+	//router.LoadHTMLGlob("views/*")
+	//
+	//router.GET("/", controller.IndexHandle)
+	////发布文章页面
+	//router.GET("/article/new/", controller.NewArticle)
+	////文章提交接口
+	//router.POST("/article/submit/", controller.ArticleSubmit)
+	////文章详情页
+	//router.GET("/article/detail/", controller.ArticleDetail)
+	//
+	////文件上传接口
+	//router.POST("/upload/file/", controller.UploadFile)
+	//
+	////留言页面
+	//router.GET("/leave/new/", controller.LeaveNew)
+	////关于我页面
+	//router.GET("/about/me/", controller.AboutMe)
+	//
+	////文章评论相关
+	//router.POST("/comment/submit/", controller.CommentSubmit)
+	//
+	////留言相关
+	//router.POST("/leave/submit/", controller.LeaveSubmit)
+	////分类下面的文章列表
+	//router.GET("/category/", controller.CategoryList)
+	//router.Run(":8080")
 }
